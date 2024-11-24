efef7d0806d8ad08963b8d108b3f265924f1d74f
Kha Dinh <dalo2903@gmail.com>
2024-11-24 15:55:52 +0900

    feat: Show LSP progress bar for zotero backend

--- bibli_ls/backends/zotero_backend.py
+++ bibli_ls/backends/zotero_backend.py
@@ -1,14 +1,15 @@
 from bibtexparser.middlewares.names import List
+from lsprotocol.types import (
+    WorkDoneProgressBegin,
+    WorkDoneProgressEnd,
+    WorkDoneProgressReport,
+)
+from pygls.progress import Progress
 from pyzotero.zotero import Zotero
 from bibtexparser.library import Library
 from bibli_ls.backends.backend import BibliBackend
 from bibli_ls.bibli_config import BackendZoteroAPIConfig, BibliBibDatabase
-from bibli_ls.server import LanguageServerProtocol
 from bibtexparser import bibtexparser
-
-# zot = zotero.Zotero(library_id, library_type, api_key)
-# we've retrieved the latest five top-level items in our library
-# we can print each item's item type and ID
 
 
 class ZoteroBackend(BibliBackend):
@@ -20,28 +21,41 @@ class ZoteroBackend(BibliBackend):
         )
         self._zot = Zotero(config.library_id, config.library_type, config.api_key)
         self._lsp = lsp
-        # self.print_items()
 
     def get_libraries(
         self,
     ) -> List[BibliBibDatabase]:
         count = self._zot.count_items()
         loaded = 0
-        limit = 100
+        limit = 50
 
         library = Library()
-        while loaded < count:
-            # items = self.items(start=loaded, limit=limit, content="bibtex")
-            items = self._zot.items(start=loaded, limit=limit, content="bibtex")
 
-            # if len(items) == 0:
-            #     break
+        progress = Progress(self._lsp)
+        progress.create("bibli")
+        progress.begin(
+            "bibli",
+            WorkDoneProgressBegin(
+                title=f"Retriving online library from `{self._zot.library_id}`",
+                message="message",
+            ),
+        )
+        while loaded < count:
+            items = self._zot.items(start=loaded, limit=limit, content="bibtex")
             loaded += len(items)
             items_str = "\n".join(items)
             bibtexparser.parse_string(items_str, library=library)
 
-            self._lsp.show_message(
-                f"Retrieved {loaded}/{count} entries from library `{self._zot.library_id}`"
+            progress.report(
+                "bibli",
+                WorkDoneProgressReport(
+                    message="Libraries loaded",
+                    percentage=int(loaded * 100 / count),
+                ),
             )
+        progress.end(
+            "bibli",
+            WorkDoneProgressEnd(message="Done"),
+        )
 
         return [BibliBibDatabase(library, None)]
